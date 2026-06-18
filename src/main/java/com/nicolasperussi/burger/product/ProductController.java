a099b8816c54f65d786b8bac9527a44a1e925c14
Nicolas Perussi <nicolasperussi12@hotmail.com>
2026-06-18 10:43:31 -0300

    feat(product): create route for creating products

--- src/main/java/com/nicolasperussi/burger/product/ProductController.java
+++ src/main/java/com/nicolasperussi/burger/product/ProductController.java
@@ -1,13 +1,16 @@
 package com.nicolasperussi.burger.product;
 
+import com.nicolasperussi.burger.product.dto.CreateProductRequest;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/product")
@@ -19,5 +22,12 @@ public class ProductController {
     public ResponseEntity<Page<Product>> findAll(@PageableDefault(size = 10, page = 0, sort = "category") Pageable pageable) {
         Page<Product> products = this.service.findAll(pageable);
         return ResponseEntity.ok(products);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Void> create(@Valid @RequestBody CreateProductRequest data, UriComponentsBuilder uriBuilder) {
+        Product product = this.service.create(data);
+        URI uri = uriBuilder.path("/product/{id}").buildAndExpand(product.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
